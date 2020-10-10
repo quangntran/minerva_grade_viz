71e7f5428d46b5a6cd1988a3765dc1d942473314
QUANG TRAN <quangtran0698@gmail.com>
2020-10-10 13:28:09 -0300

    link to chart.html

--- hello/views.py
+++ hello/views.py
@@ -8,13 +8,16 @@ def index(request):
     # r = requests.get('http://httpbin.org/status/418')
     # print(r.text)
     # return HttpResponse('<pre>' + r.text + '</pre>')
-    return render(request, "index.html")
+    context = {'Tokyo': [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 40, 23.3, 18.3, 13.9, 9.6]}
+    return render(request, "index.html", context)
 
+def view_teapot(request):
+     return render(request, "chart.html")
+    
 def db(request):
 
     greeting = Greeting()
     greeting.save()
 
     greetings = Greeting.objects.all()
-
     return render(request, "db.html", {"greetings": greetings})
