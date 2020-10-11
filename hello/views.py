db6fb5c56009693761a6bc39b1ae3239a407530c
QUANG TRAN <quangtran0698@gmail.com>
2020-10-11 11:03:02 -0300

    add stragiht line

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#from django.utils import simplejson
 
 from .models import Greeting
 import requests
@@ -12,7 +13,14 @@ def index(request):
     return render(request, "index.html", context)
 
 def view_teapot(request):
-     return render(request, "chart.html")
+    context = {'data': [
+                       {'y': 107, 'color':'red'},
+                       {'y':31, 'color':'red'},
+                       {'y':635},
+                       {'y':203, 'color':'green'},
+                       {'y':2}
+                   ]}
+    return render(request, "chart.html", context)
     
 def db(request):
 
