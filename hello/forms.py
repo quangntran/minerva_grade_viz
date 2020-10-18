d655a0a6c4801defc3a71120e94382032c6e8905
QUANG TRAN <quangtran0698@gmail.com>
2020-10-18 13:12:48 -0300

    add link to download sample csv

--- hello/forms.py
+++ hello/forms.py
@@ -3,11 +3,11 @@ from django import forms
 class GradeForm(forms.Form):
     
     file = forms.FileField(label='Upload file', label_suffix="")
-    lo_list = forms.CharField(label='LO list', max_length=500,label_suffix="")
-    co_list = forms.CharField(label='CO list', max_length=500, label_suffix="")
-    assignment_title = forms.CharField(label='Assignment titles', max_length=500, label_suffix="")
+    lo_list = forms.CharField(label='LO list', max_length=500,label_suffix="", widget=forms.Textarea(attrs={'rows':4, 'cols':15}))
+    co_list = forms.CharField(label='CO list', max_length=500, label_suffix="", widget=forms.Textarea(attrs={'rows':4, 'cols':15}))
+    assignment_title = forms.CharField(label='Assignment titles', max_length=500, label_suffix="", widget=forms.Textarea(attrs={'rows':4, 'cols':15}))
     assignment_weight = forms.CharField(label='Assignment weights', max_length=500, label_suffix="")
-    default_lo = forms.CharField(label='LO to highlight in LO evolution graph', max_length=500, label_suffix="")
+    default_lo = forms.CharField(label='LO to highlight in LO evolution graph', max_length=500, label_suffix="", widget=forms.Textarea(attrs={'rows':4, 'cols':15}))
     
     
     
